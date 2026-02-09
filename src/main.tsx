975424daf7ed4f6e46d13310798debcb24d5784b
Clemens Kaiser <kaisercgn@gmail.com>
2026-02-09 23:12:04 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
-iimport React from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom' // <- Wichtig: Der Import
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './styles.css'
 
